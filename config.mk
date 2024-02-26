b26587ac734cb0c050b14139d355f51dd40df762
lahaina <nattolecats@gmail.com>
2024-02-27 02:22:24 +0900

    sh-fwk: Start sepolicy compile

--- config.mk
+++ config.mk
@@ -11,5 +11,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     vendor/sh-fwk/privapp-permissions-2by2.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-2by2.xml
 
+# SEPolicy
+BOARD_SEPOLICY_DIRS += vendor/sh-fwk/sepolicy
+
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Inherit from proprietary blobs
 $(call inherit-product, vendor/sh-fwk/prebuilts/prebuilts-vendor.mk)
