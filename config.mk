cf76f9580c629736cdfffa3f6327d5b7805c2bd1
lahaina <nattolecats@gmail.com>
2024-02-26 22:26:06 +0900

    sh-fwk: Initial commit

---
+++ config.mk
@@ -0,0 +1,10 @@
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/sh-fwk
+
+# Required main framework
+PRODUCT_BOOT_JARS += \
+    sh-fwk
+
+# Inherit from proprietary blobs
+$(call inherit-product, vendor/sh-fwk/prebuilts/prebuilts-vendor.mk)
